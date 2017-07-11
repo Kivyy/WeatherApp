e0dc6ea6ce780d15a6512331465be651955f3ce7
Kiv Lo <kivlokw@gmail.com>
2017-07-11 14:59:02 -0400

    successfully setup api call to forecast.io

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 const request = require('request');
 const yargs = require('yargs');
 
-const argv = yargs
-  .options({
-  a: {
-    demand: true,
-    alias: 'address',
-    describe: 'Address to fetch weather for',
-    string: true,
-  }
-})
-.help()
-.alias('help', 'h')
-.argv;
+const geocode = require('./geocode/geocode')
 
-let encodedAdd = encodeURIComponent(argv.a)
+// const argv = yargs
+//   .options({
+//   a: {
+//     demand: true,
+//     alias: 'address',
+//     describe: 'Address to fetch weather for',
+//     string: true,
+//   }
+// })
+// .help()
+// .alias('help', 'h')
+// .argv;
+//
+// geocode.geocodeAddress(argv.a, (errMessage,results) => {
+//   if (errMessage){
+//     console.log(errMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2))
+//   }
+// });
+
+// 7e532c7f86e2d7f23479c9ad53882e35
 
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAdd}`,
-  json: true
-}, (err,res,body)=> {
-  if(err){
-    console.log("Unable to connect to Google servers.");
-  }else if (body.status === 'ZERO_RESULTS'){
-    console.log("Unable to find that address");
-  } else if(body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+  url: "https://api.darksky.net/forecast/7e532c7f86e2d7f23479c9ad53882e35/39.9350642,-75.1516194",
+  json: true,
+},(err,result,body) => {
+  if(!err && result.statusCode === 200) {
+    console.log(body.currently.temperature);
+  } else {
+    console.log('Unable to fetch weather.')
   }
-});
+})
